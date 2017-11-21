8dd7208bbb9f8c0434543c46d4cd6df7669022f7
Agustin <acalatroni@gmail.com>
2017-11-21 11:15:31 -0800

    Update App

--- index.R
+++ index.R
@@ -6,12 +6,12 @@ pacman::p_load(tidyverse)
 pacman::p_load(trelliscopejs)
 
 ## Import Data
-Artists  <- read_csv("https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artists.csv")
-Artworks <- read_csv("https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.csv")
+Artsts1  <- read_csv("https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artists.csv")
+Artwrks1 <- read_csv("https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.csv")
 
 
 ## Subset data
-Artworks2 <- Artworks %>% 
+Artwrks2 <- Artwrks1 %>% 
   select(Title, ThumbnailURL, URL, 
          Artist, ConstituentID, Nationality, Gender,
          Medium, Classification, Department,
@@ -21,17 +21,15 @@ Artworks2 <- Artworks %>%
          URL_artist = paste0("https://www.moma.org/artists/",ConstituentID),
          URL_piece = as.character(URL) %>% str_replace("http://","https://")) %>% 
   filter(URL_piece != "NA") %>%
-  filter(Medium == "Oil on canvas") %>% 
   mutate(panel = img_panel(ThumbnailURL),
          URL_piece = ifelse(is.na(URL_piece), "http://www.moma.org/collection/works", URL_piece)) %>% 
-  select(-ConstituentID)
+  select(-ConstituentID) 
 
-## Run Trelliscope 
-Artworks2  %>% 
+Artwrks2 %>% 
+  # run trelliscope
   trelliscope(name = "MOMA Collection",
               desc = "The Museum of Modern Art (MoMA) Collection",
               nrow = 3, ncol = 7,
               state = list(labels = c("Artist","URL_artist","Title","URL_piece")),
-              path = "moma",
-              thumb = TRUE)
+              path = "moma")
 
